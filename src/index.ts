d91112c9e5730f500d4a87365e5b5bbfdfc88276
ambrt <ambrth@gmail.com>
2021-01-28 18:31:45 +0100

    distrun added

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,17 @@ import joplin from 'api';
 import { MenuItemLocation, ToolbarButtonLocation } from 'api/types';
 
 joplin.plugins.register({
-	onStart: async function() {
+	onStart: async function () {
 		await joplin.commands.register({
 			name: 'getActiveNoteLink',
 			label: 'Copy note\'s link',
-			
+
 			execute: async () => {
-		
-		const note = await joplin.workspace.selectedNote();
-		navigator.clipboard.writeText(`[${note.title}](:/${note.id})`)
+
+				const note = await joplin.workspace.selectedNote();
+				navigator.clipboard.writeText(`[${note.title}](:/${note.id})`)
 			}
 		})
-		await joplin.views.menuItems.create('getActiveNoteLinkMenu','getActiveNoteLink', MenuItemLocation.EditorContextMenu);
+		await joplin.views.menuItems.create('getActiveNoteLinkMenu', 'getActiveNoteLink', MenuItemLocation.EditorContextMenu);
 	},
 });
